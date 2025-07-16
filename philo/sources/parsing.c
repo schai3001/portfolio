58826a95935d2cafc7b5f1f5158f0288c0e5849e
Sann CHAI <sannchaileochai@gmail.com>
2025-07-16 17:26:28 +0200

    philo

---
+++ philo/sources/parsing.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schai <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 11:35:18 by schai             #+#    #+#             */
+/*   Updated: 2022/11/11 18:06:13 by schai            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static bool	contains_only_digits(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+int	integer_atoi(char *str)
+{
+	unsigned long long int	nb;
+	int						i;
+
+	i = 0;
+	nb = 0;
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
+	if (nb > INT_MAX)
+		return (-1);
+	return ((int)nb);
+}
+
+bool	is_valid_input(int ac, char **av)
+{
+	int	i;
+	int	nb;
+
+	i = 1;
+	while (i < ac)
+	{
+		if (!contains_only_digits(av[i]))
+			return (msg(STR_ERR_INPUT_DIGIT, av[i], false));
+		nb = integer_atoi(av[i]);
+		if (i == 1 && (nb <= 0 || nb > MAX_PHILOS))
+			return (msg(STR_ERR_INPUT_POFLOW, STR_MAX_PHILOS, false));
+		if (i != 1 && nb == -1)
+			return (msg(STR_ERR_INPUT_DIGIT, av[i], false));
+		i++;
+	}
+	return (true);
+}
---
+++ philo/sources/time.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schai <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 11:35:18 by schai             #+#    #+#             */
+/*   Updated: 2022/11/11 18:06:13 by schai            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+time_t	get_time_in_ms(void)
+{
+	struct timeval		tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+void	philo_sleep(t_table *table, time_t sleep_time)
+{
+	time_t	wake_up;
+
+	wake_up = get_time_in_ms() + sleep_time;
+	while (get_time_in_ms() < wake_up)
+	{
+		if (has_simulation_stopped(table))
+			break ;
+		usleep(100);
+	}
+}
+
+void	sim_start_delay(time_t start_time)
+{
+	while (get_time_in_ms() < start_time)
+		continue ;
+}
